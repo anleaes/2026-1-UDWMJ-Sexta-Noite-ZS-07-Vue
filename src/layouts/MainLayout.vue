4f56fc27ae326bd2596138e1049486a772778864
¨thaisgmaier¨ <thaisgmaier@outlook.com>
2026-06-08 11:19:14 -0300

    initial commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,28 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-header>
+      <q-toolbar class="bg-primary" style="height: 90px">
+        <q-space />
+        <q-toolbar-title style="color: #444441">
+          <q-icon name="favorite" size="lg" style="color: #c60102" />
+          Adotar&Amar
+        </q-toolbar-title>
+
+        <q-space />
+
+        <q-btn no-caps style="color: #444441" stretch flat label="Animais" />
+        <q-btn no-caps style="color: #444441" stretch flat label="Como adotar?" />
+        <q-btn no-caps style="color: #444441" stretch flat label="Sobre" />
+        <q-btn no-caps style="color: #444441" stretch flat label="Contato" />
+        <q-btn style="color: #444441" stretch flat icon="account_circle" />
+        <q-space />
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup></script>
